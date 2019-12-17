eca8499f149441f5b55ad505088bb1cf7bef0081
Mitchell Rhoads <mitchelldrhoads@gmail.com>
2019-12-17 14:46:03 -0800

    added portfolio

---
+++ precache-manifest.10709587850c43923efcb9661f0e26fc.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce8ad8973b32e2095f7abcc5b7a64f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe3b522c3f4d293d606",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "ec4b07c71ba4a97bf246",
+    "url": "/static/css/main.187ec791.chunk.css"
+  },
+  {
+    "revision": "efe3b522c3f4d293d606",
+    "url": "/static/js/2.16d873b0.chunk.js"
+  },
+  {
+    "revision": "ec4b07c71ba4a97bf246",
+    "url": "/static/js/main.858225c9.chunk.js"
+  },
+  {
+    "revision": "767e35795de6c22d9fb1",
+    "url": "/static/js/runtime-main.8bf9c574.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "fe6f85d6a670eea39c93239fa2297176",
+    "url": "/static/media/OrderUp.fe6f85d6.png"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/static/media/css-3.b848d5d9.png"
+  },
+  {
+    "revision": "93947f0fab1c50c3872c092a0bafab75",
+    "url": "/static/media/fineDiningsite.93947f0f.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a37eeddf89e5a9ea840cf301858d2513",
+    "url": "/static/media/javascript.a37eeddf.png"
+  },
+  {
+    "revision": "64dd08ca9b64edd224711b30b0c48088",
+    "url": "/static/media/jquery.64dd08ca.png"
+  },
+  {
+    "revision": "ca20597ffa8221f1ff5941cc98b6b34a",
+    "url": "/static/media/matrix.ca20597f.png"
+  },
+  {
+    "revision": "b10b140c6a5e314a0eccf161c5d4f112",
+    "url": "/static/media/mongodb.b10b140c.png"
+  },
+  {
+    "revision": "38b4764b906edf347113603d4042948e",
+    "url": "/static/media/mongoose.38b4764b.jpg"
+  },
+  {
+    "revision": "9abfc3165a11e9d76893534de7584b3d",
+    "url": "/static/media/mysql.9abfc316.png"
+  },
+  {
+    "revision": "92e545060c71cd7c104c71797a0bd745",
+    "url": "/static/media/node.92e54506.png"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/static/media/react.82a318cc.png"
+  },
+  {
+    "revision": "c412479861d21d42ae3e539b3b1ff158",
+    "url": "/static/media/sk8ersite.c4124798.png"
+  },
+  {
+    "revision": "288caf89d9e32474055cc737fe86f763",
+    "url": "/static/media/socket.288caf89.png"
+  }
+]);
